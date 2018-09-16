f3678db28cc5cf8ae8f8b89331c24bf248da0efe
mguevara717 <marissa.guevara717@gmail.com>
2018-09-16 14:40:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const driver = {};
+
+//returns a driver with the original key value
+//pairs the new key value pair
+function updateDriverWithKeyAndValue(driver, key, value) {
+  //non-destructive
+    return Object.assign({}, driver, {[key]: value});
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+    return driver;
+}
+
+
+function deleteFromDriverByKey(driver, key) {
+  //non destructive function, therefore I need to
+  //declare a new object and use the assign method to delete
+  const new_object = Object.assign({}, driver);
+
+  delete new_object[key];
+    return new_object;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+    return driver;
+}
